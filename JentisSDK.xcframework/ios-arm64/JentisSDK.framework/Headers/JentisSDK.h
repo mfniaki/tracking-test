8ea3ff41625d4d1b0ce0af86ee88c214af6f9742
Mohsen niaki <mohsen.niaki@xxxl.digital>
2025-08-21 11:42:42 +0330

    Add JentisSDK.xcframework and podspec

---
+++ JentisSDK.xcframework/ios-arm64/JentisSDK.framework/Headers/JentisSDK.h
@@ -0,0 +1,18 @@
+//
+//  JentisSDK.h
+//  JentisSDK
+//
+//  Created by Alexandre Oliveira on 07/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JentisSDK.
+FOUNDATION_EXPORT double JentisSDKVersionNumber;
+
+//! Project version string for JentisSDK.
+FOUNDATION_EXPORT const unsigned char JentisSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JentisSDK/PublicHeader.h>
+
+
